b76902664f01000d704ddeefbd9b5c4c6f0ae3cf
Summer Fang <summer.fang@jolli.ai>
2025-12-10 23:37:04 -0800

    Add Docusaurus documentation site

---
+++ content/_meta.ts
@@ -0,0 +1,9 @@
+export default {
+  'index': 'Home',
+  'get-start-document': 'Get start document',
+  'api-jolli': {
+    title: 'Jolli',
+    type: 'page',
+    href: '/api-docs-jolli.html'
+  }
+}
